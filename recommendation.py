8077c60772c1694ffa183ce4263672869af32003
Roberto Perez <129693538+PeJiR@users.noreply.github.com>
2024-01-02 01:38:05 +0100

    update

--- recommendation.py
+++ recommendation.py
@@ -2,9 +2,14 @@ import numpy as np
 from lightfm.datasets import fetch_movilens
 from lightfm import LightFM
 
-#fetch date and format it
-data = fetch_movilens(min_rating = 4.0)
+# Fetch and format the data
+data = fetch_movilens(min_rating=4.0)
+loader = LightFM.DataLoader(data)
 
-#printtraining and testing data
-print(repr(data['train']))
-print(repr(data['test']))
+# Create training and testing datasets
+train_data = loader.create_ratings_dataset()
+test_data = loader.create_testing_split(train_data)
+
+# Print training and testing data
+print(train_data)
+print(test_data)
